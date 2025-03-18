2194ca8e60d1194ba95fe9c5ed44c6dea36b2b28
damienrigaudeau <91134839+damienrigaudeau@users.noreply.github.com>
2025-03-18 14:47:18 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -6,3 +6,23 @@ CREATE TABLE clients (
     prenom TEXT NOT NULL,
     adresse TEXT NOT NULL
 );
+
+DROP TABLE IF EXISTS livres;
+CREATE TABLE livres (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titre TEXT NOT NULL,
+    auteur TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    disponible TEXT NOT NULL DEFAULT 'oui'
+);
+
+DROP TABLE IF EXISTS emprunts;
+CREATE TABLE emprunts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    client_id INTEGER NOT NULL,
+    livre_id INTEGER NOT NULL,
+    date_emprunt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    date_retour TIMESTAMP,
+    FOREIGN KEY (client_id) REFERENCES clients(id),
+    FOREIGN KEY (livre_id) REFERENCES livres(id)
+);
