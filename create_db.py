2ce50731c49431075890e380c392069b31ff2d2e
damienrigaudeau <91134839+damienrigaudeau@users.noreply.github.com>
2025-03-18 14:46:02 +0100

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -16,5 +16,25 @@ cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('GAGN
 cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('DUBOIS', 'Charlotte', '789, Rue des Roses, 13005 Marseille'))
 cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
 
+
+cur.execute("INSERT INTO livres (titre, auteur, genre, disponible) VALUES (?, ?, ?, ?)",
+            ('Le Petit Prince', 'Antoine de Saint-Exupéry', 'Conte philosophique', 'oui'))
+
+cur.execute("INSERT INTO livres (titre, auteur, genre, disponible) VALUES (?, ?, ?, ?)",
+            ('Les Misérables', 'Victor Hugo', 'Roman historique', 'oui'))
+
+cur.execute("INSERT INTO livres (titre, auteur, genre, disponible) VALUES (?, ?, ?, ?)",
+            ('1984', 'George Orwell', 'Science-fiction', 'oui'))
+
+cur.execute("INSERT INTO livres (titre, auteur, genre, disponible) VALUES (?, ?, ?, ?)",
+            ('Harry Potter à l\'école des sorciers', 'J.K. Rowling', 'Fantasy', 'oui'))
+
+cur.execute("INSERT INTO livres (titre, auteur, genre, disponible) VALUES (?, ?, ?, ?)",
+            ('L\'Étranger', 'Albert Camus', 'Roman', 'oui'))
+
+cur.execute("INSERT INTO livres (titre, auteur, genre, disponible) VALUES (?, ?, ?, ?)",
+            ('Voyage au bout de la nuit', 'Louis-Ferdinand Céline', 'Roman', 'oui'))
+
+
 connection.commit()
 connection.close()
